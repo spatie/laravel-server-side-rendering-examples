7ce36018e8b02eae8c7e655590cba8563d9e14b4
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2018-03-09 15:26:11 +0100

    Update react example

--- resources/views/react.blade.php
+++ resources/views/react.blade.php
@@ -5,8 +5,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel + React SSR Example</title>
+        <script defer src="{{ mix('js/react/entry-client.js') }}"></script>
     </head>
     <body>
-        {!! ssr('js/react/entry.js')->withFallback('<div id="app"></div>') !!}
+        {!! ssr('js/react/entry-server.js')->fallback('<div id="app"></div>')->render() !!}
     </body>
 </html>
